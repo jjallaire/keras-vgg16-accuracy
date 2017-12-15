345bad283cc58ee68b27ba0e1c537c2c08c0748d
JJ Allaire <jj.allaire@gmail.com>
2017-12-15 07:48:13 -0500

    Update keras-vgg16-accuracy.py

--- keras-vgg16-accuracy.py
+++ keras-vgg16-accuracy.py
@@ -1,78 +1,123 @@
 
-# Based on the code from DL w/ Python found here:
-# https://github.com/fchollet/deep-learning-with-python-notebooks/blob/master/5.3-using-a-pretrained-convnet.ipynb
 
 import os
-import keras
-from keras import models
-from keras import layers
-from keras import optimizers
-from keras.applications import VGG16
-from keras.preprocessing.image import ImageDataGenerator
-
 
 base_dir = 'cats_and_dogs_small'
 train_dir = os.path.join(base_dir, 'train')
 validation_dir = os.path.join(base_dir, 'validation')
 test_dir = os.path.join(base_dir, 'test')
 
-
+from keras.applications import VGG16
 conv_base = VGG16(weights='imagenet',
                   include_top=False,
                   input_shape=(150, 150, 3))
-                  
+
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+
+base_dir = 'cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train') 
+validation_dir = os.path.join(base_dir, 'validation') 
+test_dir = os.path.join(base_dir, 'test')
+
+datagen = ImageDataGenerator(rescale=1./255) 
+batch_size = 20
+
+def extract_features(directory, sample_count):
+  features = np.zeros(shape=(sample_count, 4, 4, 512)) 
+  labels = np.zeros(shape=(sample_count))
+  generator = datagen.flow_from_directory(
+    directory, 
+    target_size=(150, 150), 
+    batch_size=batch_size, 
+    class_mode='binary')
+  i=0
+  for inputs_batch, labels_batch in generator:
+    features_batch = conv_base.predict(inputs_batch)
+    features[i * batch_size : (i + 1) * batch_size] = features_batch 
+    labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+    i += 1
+    if i * batch_size >= sample_count:
+      break
+  return features, labels
+
+train_features, train_labels = extract_features(train_dir, 2000) 
+validation_features, validation_labels = extract_features(validation_dir, 1000) 
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+from keras import models
+from keras import layers
+from keras import optimizers
+
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+              
+history = model.fit(train_features, train_labels,
+                    epochs=30,
+                    batch_size=20,
+                    validation_data=(validation_features, validation_labels))
+                    
+
+
+from keras import models
+from keras import layers
 model = models.Sequential()
 model.add(conv_base)
 model.add(layers.Flatten())
 model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
+
 conv_base.trainable = False
 
-
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
 train_datagen = ImageDataGenerator(
-      rescale=1./255,
-      rotation_range=40,
-      width_shift_range=0.2,
-      height_shift_range=0.2,
-      shear_range=0.2,
-      zoom_range=0.2,
-      horizontal_flip=True,
-      fill_mode='nearest')
+  rescale=1./255,
+  rotation_range=40,
+  width_shift_range=0.2,
+  height_shift_range=0.2,
+  shear_range=0.2,
+  zoom_range=0.2,
+  horizontal_flip=True,
+  fill_mode='nearest')
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
-        train_dir,
-        target_size=(150, 150),
-        batch_size=20,
-        class_mode='binary')
+  train_dir,
+  target_size=(150, 150),
+  batch_size=20,
+  class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
-        validation_dir,
-        target_size=(150, 150),
-        batch_size=20,
-        class_mode='binary')
-
+  validation_dir,
+  target_size=(150, 150),
+  batch_size=20,
+  class_mode='binary')
 
 model.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=2e-5),
               metrics=['acc'])
 
-# This gets to ~ 90% validation accuracy
 history = model.fit_generator(
-      train_generator,
-      steps_per_epoch=100,
-      epochs=30,
-      validation_data=validation_generator,
-      validation_steps=50,
-      verbose=1)
-      
-
-# Fine Tune
+  train_generator,
+  steps_per_epoch=100,
+  epochs=30,
+  validation_data=validation_generator,
+  validation_steps=50)
 
 conv_base.trainable = True
-
 set_trainable = False
 for layer in conv_base.layers:
     if layer.name == 'block5_conv1':
@@ -82,15 +127,15 @@ for layer in conv_base.layers:
     else:
         layer.trainable = False
         
-        
 model.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-5),
               metrics=['acc'])
-
-# This gets to ~ 93 % validation accuracy
+                      
 history = model.fit_generator(
       train_generator,
       steps_per_epoch=100,
       epochs=100,
       validation_data=validation_generator,
       validation_steps=50)
+      
+    
